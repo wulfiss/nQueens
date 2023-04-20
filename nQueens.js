2e4c2f4d8d8c73d30a79dfbce06d60d87112d1dd
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-20 15:13:12 -0300

    Add explanations

--- nQueens.js
+++ nQueens.js
@@ -1,45 +1,10 @@
-const knightMoves = (start, end) => {
-  const moves = [
-    [-2, -1],
-    [-1, -2],
-    [1, -2],
-    [2, -1],
-    [-2, 1],
-    [-1, 2],
-    [1, 2],
-    [2, 1],
-  ];
-
-  const visited = new Set();
-  const queue = [[start]];
-
-  while (queue.length > 0) {
-    const path = queue.shift();
-    const last = path[path.length - 1];
-    if (last[0] === end[0] && last[1] === end[1]) {
-      let mgs = `The Knight reached the square in ${path.length - 1} steps. `;
-      return { mgs, path };
-    }
-    if (visited.has(last.toString())) {
-      continue;
-    }
-    visited.add(last.toString());
-    for (const move of moves) {
-      const next = [last[0] + move[0], last[1] + move[1]];
-      if (next[0] >= 0 && next[0] <= 7 && next[1] >= 0 && next[1] <= 7) {
-        queue.push(path.concat([next]));
-      }
-    }
-  }
-  return null;
-};
-
 const random = (num) => {
   return Math.floor(Math.random() * num);
 };
 
 const nQueens = (x = 8, y = 8, xS = random(x), yS = random(y)) => {
   const moves = [
+    //all the direction of the move that the queen can do;
     [-1, 1],
     [0, 1],
     [1, 1],
@@ -50,11 +15,12 @@ const nQueens = (x = 8, y = 8, xS = random(x), yS = random(y)) => {
     [-1, 0],
   ];
 
-  const invalid = new Set();
-  const start = [xS, yS];
-  const queue = [start];
-  let positions = [];
+  const invalid = new Set(); //set a Set for the invalid squares
+  const start = [xS, yS]; //user start or random start
+  const queue = [start]; // all the position in the board are queue here
+  let positions = []; //all the valid position of the Queen are stacked here
 
+  //generate the board i = X, j = y
   for (let i = 0; i < x; i += 1) {
     for (let j = 0; j < y; j += 1) {
       queue.push([i, j]);
@@ -62,12 +28,13 @@ const nQueens = (x = 8, y = 8, xS = random(x), yS = random(y)) => {
   }
 
   while (queue.length > 0) {
-    let current = queue.shift();
+    let current = queue.shift(); // desqueue the first element
 
     if (invalid.has(current.toString())) {
+      //if is in the invalid set, will jump to the next element.
       continue;
     }
-
+    //if not the element is push to the position arr, and all his movements inside the board are calculates and saved in the invalid set.
     positions.push(current);
     for (const move of moves) {
       let next = current;
@@ -79,4 +46,5 @@ const nQueens = (x = 8, y = 8, xS = random(x), yS = random(y)) => {
   }
   return positions;
 };
+
 export { nQueens };
